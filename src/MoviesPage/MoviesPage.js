f1a3833b0c348c10b56951b825608255916c34ba
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-18 20:29:36 -0700

    Create boiler for MoviePage

---
+++ src/MoviesPage/MoviesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieThumbnail from '../MovieThumbnail/MovieThumbnail.js'
+
+const MoviesPage = () => {
+  return (
+    <section>
+      <h1>Movies...</h1>
+      <MovieThumbnail />
+    </section>
+  )
+}
+
+export default MoviesPage;
