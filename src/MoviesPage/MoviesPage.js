3391b11d0ded32433ffefc58e32a232150228cda
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-20 14:39:59 -0700

    Connect moviespage to store

--- src/MoviesPage/MoviesPage.js
+++ src/MoviesPage/MoviesPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import MovieThumbnail from '../MovieThumbnail/MovieThumbnail.js'
+import MovieThumbnail from '../MovieThumbnail/MovieThumbnail.js';
+import './MoviesPage.scss';
+import { connect } from 'react-redux';
 
-const MoviesPage = () => {
+const MoviesPage = ({ movies }) => {
+  const listOfMovies = movies.map(movie => <MovieThumbnail {...movie}/>)
   return (
-    <section>
+    <section className='movies-page'>
+      <div className='film-block'></div>
       <h1>Movies...</h1>
-      <MovieThumbnail />
+      {listOfMovies}
     </section>
   )
 }
 
-export default MoviesPage;
+const mapStateToProps = state => ({
+  movies: state.movies,
+})
+
+export default connect(mapStateToProps)(MoviesPage);
