23f40804341f19627e43c08d573b8ae109a08679
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-20 16:56:21 -0700

    Add logout func & user name display

--- src/App/App.js
+++ src/App/App.js
@@ -20,6 +20,10 @@ class App extends Component {
       .catch(err => err)
   }
 
+  logout = () => {
+    window.location.href = '/';
+  }
+
   render() {
     return (
       <main className="App">
@@ -28,7 +32,12 @@ class App extends Component {
             <div className='film-icon'></div>
             <div>Rancid Tomas</div>
           </div>
-          <NavLink className='login-btn' to='/login' type='button'>Login</NavLink>
+          {this.props.user.name ? 
+            <div>
+              <h3>Welcome, {this.props.user.name}</h3>
+              <button onClick={this.logout}>Logout</button>
+            </div>:
+            <NavLink className='login-btn' to='/login' type='button'>Login</NavLink>}
         </nav>
         <Route exact path='/login' component={Login} />
         <Route exact path='/' component={MoviesPage} />
@@ -39,6 +48,7 @@ class App extends Component {
 
 const mapStateToProps = state => ({
   movies: state.movies,
+  user: state.user,
 })
 
 const mapDispatchToProps = dispatch => ({
