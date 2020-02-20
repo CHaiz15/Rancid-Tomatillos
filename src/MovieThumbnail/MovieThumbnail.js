a424a04e28f5c58d81a54546d12052d75deab815
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-20 16:57:26 -0700

    Add movie thumbnail display

--- src/MovieThumbnail/MovieThumbnail.js
+++ src/MovieThumbnail/MovieThumbnail.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './MovieThumbnail.scss'
 
-const MovieThumbnail = ({ title }) => {
+const MovieThumbnail = ({ title, average_rating, poster_path }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-    </div>
+    <span className='movie-thumbnail'>
+      <img className='movie-img' src={poster_path} alt={title + ' image'}></img>
+      <h3>Rating: {average_rating}</h3>
+    </span>
   )
 }
 
