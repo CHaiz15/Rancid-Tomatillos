1a8363950878d32817c7b17ea14c985f21ca435c
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-20 16:29:57 -0700

    Set up Link for movieDetails

--- src/MoviesPage/MoviesPage.js
+++ src/MoviesPage/MoviesPage.js
@@ -2,9 +2,13 @@ import React from 'react';
 import MovieThumbnail from '../MovieThumbnail/MovieThumbnail.js';
 import './MoviesPage.scss';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const MoviesPage = ({ movies }) => {
-  const listOfMovies = movies.map(movie => <MovieThumbnail {...movie}/>)
+  const listOfMovies = movies.map(movie => {
+    const { id } = movie;
+    return <Link to={`/movies/${id}`}><MovieThumbnail {...movie}/></Link>
+  })
   return (
     <section className='movies-page'>
       <div className='film-block'></div>
