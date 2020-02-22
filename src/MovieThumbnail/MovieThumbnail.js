f70ccb374c201e7d586a6fcd49b32d3fd8db8c46
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-21 18:25:59 -0700

    Remove className

--- src/MovieThumbnail/MovieThumbnail.js
+++ src/MovieThumbnail/MovieThumbnail.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './MovieThumbnail.scss'
 
-const MovieThumbnail = ({ title, average_rating, poster_path }) => {
+const MovieThumbnail = ({ title, average_rating, poster_path, ratings }) => {
   return (
-    <span className='movie-thumbnail'>
+    <span>
       <img className='movie-img' src={poster_path} alt={title + ' image'}></img>
       <h3>Rating: {average_rating}</h3>
     </span>
