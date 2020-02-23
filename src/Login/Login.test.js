9a0815aee239171de560f3b9d1a97d98f20659d8
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-22 22:31:57 -0700

    Add snapshot

---
+++ src/Login/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Login, mapStateToProps, mapDispatchToProps } from './Login';
+import { shallow } from 'enzyme';
+
+describe('Login', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<Login />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
