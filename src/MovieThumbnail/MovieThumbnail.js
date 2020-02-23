2e8a3713d931ce36a3952c49200ee0223a831501
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-23 15:26:48 -0700

    Add round to rating number

--- src/MovieThumbnail/MovieThumbnail.js
+++ src/MovieThumbnail/MovieThumbnail.js
@@ -5,7 +5,7 @@ const MovieThumbnail = ({ title, average_rating, poster_path, ratings }) => {
   return (
     <span>
       <img className='movie-img' src={poster_path} alt={title + ' image'}></img>
-      <h3>Rating: {average_rating}</h3>
+      <h3>Rating: {Math.round(average_rating)}</h3>
     </span>
   )
 }
