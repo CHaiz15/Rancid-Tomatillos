f780622c181e14d972c91f1aac08f1497f14b853
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-23 16:40:00 -0700

    Add classes for styling

--- src/MovieThumbnail/MovieThumbnail.js
+++ src/MovieThumbnail/MovieThumbnail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './MovieThumbnail.scss'
 
-const MovieThumbnail = ({ title, average_rating, poster_path, ratings }) => {
+const MovieThumbnail = ({ title, average_rating, backdrop_path, ratings }) => {
   return (
     <span>
-      <img className='movie-img' src={poster_path} alt={title + ' image'}></img>
-      <h3>Rating: {average_rating}</h3>
+      <img className='movie-img' src={backdrop_path} alt={title + ' image'}></img>
+      <div className='thumbnail-txt'>
+        <h3 className='thumbnail-title'>{title}</h3>
+        <h3 className='thumbnail-rating'>Rating: {Math.round(average_rating)}</h3>
+      </div>
     </span>
   )
 }
