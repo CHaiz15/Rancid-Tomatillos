cefa3da416625c193b6605348d063397921310c5
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-24 13:20:50 -0700

    Change movieDetails to class component

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MovieDetails.scss';
 import { connect } from 'react-redux';
+import { postRating } from '../apiCalls';
 
-const MovieDetails = ({ id, title, poster_path, backdrop_path, release_date, overview, average_rating, user, ratings }) => {
-  let foundRating = 0;
-  
-  if(!user.loggedOut && ratings.find(currentRating => currentRating.movie_id === id)) {
-    foundRating = ratings.find(currentRating => currentRating.movie_id === id).rating
+export class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentMovieRating: 0,
+    }
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({currentMovieRating: parseInt(e.target.value)})
   }
   
-  const ratingsDisplay = () => {
+  ratingsDisplay = (user, average_rating) => {
     if(user.loggedOut)  {
       return <h3>Avg. Rating: {Math.round(average_rating)}</h3>
     } else {
       return (
         <div className='ratings-container'>
           <h3>Avg. Rating: {Math.round(average_rating)}</h3>
-          {foundRating === 0 ? 
             <div className='add-rating-box'>
+            {this.state.currentMovieRating !== 0 && <h3>My Rating: {this.state.currentMovieRating}</h3>} 
+              <form>
               <h3>Add Rating: </h3>  
-              <select>
-                <option value="">Rating</option>
+              <select value={this.state.currentMovieRating} onChange={(e) => this.handleChange(e)}>
+                {/* <option value="">Rating</option> */}
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -32,26 +40,35 @@ const MovieDetails = ({ id, title, poster_path, backdrop_path, release_date, ove
                 <option value="9">9</option>
                 <option value="10">10</option>
               </select>
-            </div>:
-            <h3>My Rating: {foundRating}</h3>
-          }
+              {/* <input type='button' onClick={() = } value='Rate'/>             */}
+              </form>
+            </div>
         </div>
       )
     }
   }
 
-  return (
-    <article className='movie-details-pg' >
+  componentDidMount = () => {
+    if(!this.props.user.loggedOut && this.props.ratings.find(currentRating => currentRating.movie_id === this.props.id)) {
+      this.setState({currentMovieRating: this.props.ratings.find(currentRating => currentRating.movie_id === this.props.id).rating})
+    }
+  }
+
+  render() {
+    const { id, title, poster_path, backdrop_path, release_date, overview, average_rating, user, ratings } = this.props
+    return (
+     <article className='movie-details-pg' >
       <div className='float-img' style={{backgroundImage: `url(${backdrop_path})`}}></div>
       <section className='movie-info'>
         <h2 className='detail-title'>{title}</h2>
         <p className='detail-release'>Released: {release_date}</p>
         <h4 className='detail-overview'>{overview}</h4>
-        <h4>{ratingsDisplay()}</h4> 
+        <h4>{this.ratingsDisplay(user, average_rating, ratings, id)}</h4> 
       </section>
       <img className='detail-img' alt={title + 'image'}src={poster_path}/>
-    </article>
-  )
+     </article>
+    ) 
+  }
 }
 
 const mapStateToProps = state => ({
