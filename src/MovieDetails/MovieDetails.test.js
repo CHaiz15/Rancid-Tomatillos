08e4fb28ead281e71ddd917382d76278d7e504b6
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-25 15:48:43 -0700

    Add test
    Co-authored-by: Caleb Haizlett <caleb.haizlett@gmail.com>

--- src/MovieDetails/MovieDetails.test.js
+++ src/MovieDetails/MovieDetails.test.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { MovieDetails, mapStateToProps, mapDispatchToProps } from './MovieDetails';
 import { shallow } from 'enzyme';
+import { deleteRating } from '../apiCalls'
 
 describe('MovieDetails', () => {
-  let mockUser,mockRatings
-  it('should match the snapshot', () => {
+  let wrapper,mockUser,mockRatings,mockEvent,mockId
+  beforeEach(() => {
     mockUser = {
       name: 'Ken',
       id: 23
     }
     mockRatings = [
-      { name: 'Sonic', rating: 6 },
-      { name: 'Parasite', rating: 9 }
+      { movie_id: 23, name: 'Sonic', rating: 6 },
+      { movie_id: 24, name: 'Parasite', rating: 9 }
     ]
-    const wrapper = shallow(<MovieDetails user={mockUser} ratings={mockRatings}/>);
+    mockId = 23
+    wrapper = shallow(<MovieDetails id={mockId} user={mockUser} ratings={mockRatings}/>);
+  })
+  it('should match the snapshot', () => {
+
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should handleChange', () =>{
+    mockEvent = {
+      preventDefault: jest.fn(),
+      target: {
+        value: 7
+      }
+    }
+    wrapper.instance().handleChange(mockEvent)
+    expect(wrapper.state('currentMovieRating')).toEqual(7)
+  })
+
+  it('should call updateRatings on click', () =>{
+    wrapper.instance().updateRatings = jest.fn()
+    wrapper.find('input').simulate('click')
+    expect(wrapper.instance().updateRatings).toHaveBeenCalled()
+  })
 });
